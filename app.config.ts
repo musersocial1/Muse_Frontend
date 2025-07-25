d78371cdc87a667e50d5b0c05d29ade852d0de12
Davis Isibor <davisisibor@gmail.com>
2025-07-25 09:13:23 +0100

    update

--- app.config.ts
+++ app.config.ts
@@ -49,37 +49,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   extra: {
     router: {},
     eas: {
-      projectId: "a094832f-e025-4f8d-b5f7-7548fa7c60be",
+      projectId: "f50076b3-5599-4012-8781-3975a632a12b",
     },
-    EXPO_PUBLIC_APP_NAME: process.env.EXPO_PUBLIC_APP_NAME,
-    EXPO_PUBLIC_APP_VERSION: process.env.EXPO_PUBLIC_APP_VERSION,
-    EXPO_PUBLIC_APP_ENV: process.env.EXPO_PUBLIC_APP_ENV,
-    EXPO_PUBLIC_APP_SCHEME: process.env.EXPO_PUBLIC_APP_SCHEME,
-    EXPO_PUBLIC_BUNDLE_ID: process.env.EXPO_PUBLIC_BUNDLE_ID,
-
-    EXPO_PUBLIC_USER_API_URL: process.env.EXPO_PUBLIC_USER_API_URL,
-    EXPO_PUBLIC_POST_API_URL: process.env.EXPO_PUBLIC_POST_API_URL,
-    EXPO_PUBLIC_API_VERSION: process.env.EXPO_PUBLIC_API_VERSION,
-    EXPO_PUBLIC_API_TIMEOUT: Number(process.env.EXPO_PUBLIC_API_TIMEOUT),
-    EXPO_PUBLIC_MAX_RETRY_ATTEMPTS: Number(
-      process.env.EXPO_PUBLIC_MAX_RETRY_ATTEMPTS
-    ),
-
-    EXPO_PUBLIC_STORAGE_PREFIX: process.env.EXPO_PUBLIC_STORAGE_PREFIX,
-
-    EXPO_PUBLIC_ENABLE_ANALYTICS:
-      process.env.EXPO_PUBLIC_ENABLE_ANALYTICS === "true",
-    EXPO_PUBLIC_ENABLE_CRASH_REPORTING:
-      process.env.EXPO_PUBLIC_ENABLE_CRASH_REPORTING === "true",
-    EXPO_PUBLIC_ENABLE_DEBUG_MODE:
-      process.env.EXPO_PUBLIC_ENABLE_DEBUG_MODE === "true",
-
-    EXPO_PUBLIC_IS_DEV: process.env.EXPO_PUBLIC_IS_DEV === "true",
-    EXPO_PUBLIC_IS_PROD: process.env.EXPO_PUBLIC_IS_PROD === "true",
-    EXPO_PUBLIC_IS_STAGING: process.env.EXPO_PUBLIC_IS_STAGING === "true",
-
-    EXPO_PUBLIC_EMAIL_LINK: process.env.EXPO_PUBLIC_EMAIL_LINK,
-    // You can expose env vars here if needed:
-    // API_URL: process.env.API_URL
   },
 });
